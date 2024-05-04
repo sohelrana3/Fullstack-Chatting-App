3bc1697d31625efc3b290421d0f82e7090fe2f88
Sohel Rana <expartdesigns5@gmail.com>
2024-05-04 08:23:36 +0600

    index js file create

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello node js");
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { log } = require("console");
+
+const PORT = process.env.PORT || 8000;
+
+//middleware start
+const app = express();
+app.use(express.json());
+app.use(cors);
+// middleware end
+
+const server = http.createServer(app);
+server.listen(PORT, () => {
+    console.log(`Server is running ${PORT}`);
+});
